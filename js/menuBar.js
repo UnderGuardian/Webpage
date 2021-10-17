d3087ff5d31f6a06359ff653554aadbe5d6ea90d
Mark Miller <47294360+UnderGuardian@users.noreply.github.com>
2021-10-17 17:27:25 -0500

    Menu Bar Social Media Icons

--- js/menuBar.js
+++ js/menuBar.js
@@ -23,13 +23,24 @@ menuBarDiv.innerHTML = `
         </a>
       </div>
       <div class="socialContainer">
-        <div class="circle">
-          <div id="facebookLogo"></div>
-          <div id="facebookMiddle"></div>
+        <div class="circle" onclick="window.open('https://www.facebook.com/VideojuegosFrostBolt/');">
+          <div id="logoContainer">
+              <svg id="facebookLogo" viewBox="0 0 6084 11449">
+                <path
+                  d="M 5685,6390 6000,4334 H 4028 V 3000 c 0,-562 275,-1111 1159,-1111 h 897 V 139 C 6084,139 5270,0 4492,0 2868,0 1806,984 1806,2767 V 4334 H 0 v 2056 h 1806 v 4969 c 362,57 733,86 1111,86 378,0 749,-30 1111,-86 V 6390 Z"
+                  style="fill: var(--primary-color)"/>
+              </svg>
+          </div>
         </div>
       </div>
-      <div class="socialContainer">
-        <!-- <div class="circle"></div> -->
+      <div class="socialContainer" style="margin-left: 7px; margin-right: 14px;">
+        <div class="circle" onclick="window.open('https://www.instagram.com/frostbolt_/')">
+          <div id="logoContainer">
+              <div id="outerCage"></div>
+              <div id="dot"></div>
+              <div id="innerCircle"></div>
+          </div>
+        </div>
       </div>
     </div>`;
 
